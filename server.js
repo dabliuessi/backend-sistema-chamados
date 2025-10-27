7465c715e91df286786c42f3de64a07c58e14e41
dabliuessi <wsmdm555@gmail.com>
2025-10-27 19:27:11 -0300

    commit01

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json()); // essencial para req.body
 app.use(cors({
-  origin: "http://localhost:5174",
+  origin: "http://localhost:5173",
   methods: ["GET", "POST", "PATCH", "DELETE"],
   credentials: true,
 }));
