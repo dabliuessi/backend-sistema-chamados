065e5895518e4b9f54031b4b14bdcbe675080009
dabliuessi <wsmdm555@gmail.com>
2025-12-09 19:49:09 -0300

    Add editarChamado route to chamadoRoutes

--- routes/chamadoRoutes.js
+++ routes/chamadoRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { abrirChamado, verMeusChamados, verTodosChamados, atualizarStatus, excluirChamado } from '../controllers/chamadoController.js';
+import { abrirChamado, verMeusChamados, verTodosChamados, atualizarStatus, excluirChamado, editarChamado } from '../controllers/chamadoController.js';
 import autenticar from '../middleware/autenticar.js';
 
 const router = express.Router();
@@ -10,5 +10,6 @@ router.get('/meus', verMeusChamados);
 router.get('/todos', verTodosChamados);
 router.patch('/:id/status', atualizarStatus);
 router.delete('/:id', excluirChamado);
+router.put('/:id', editarChamado);
 
 export default router;
